b1e0aa59638298a4ee75963c743fea194b827583
Niv Sardi <xaiki@evilgiggle.com>
2018-08-26 09:13:15 -0300

    add simple run script
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+export HACKNIT_GITHUB_KEY = ""
+
+REPOS=hacknit-seguranca hacknit-educacao hacknit-saude hacknit-voluntariado hacknit-mobilidadeurbana hacknit-meioambiente hacknit-conservacao
+
+for repo in ${REPOS}; do
+        HACKNIT_REPO=${REPO} node index.js
+done
